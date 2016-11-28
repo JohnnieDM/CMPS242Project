8edf4f77e91d5091e89121245d6f1ae63e515253
robert501128 <robert501128@gmail.com>
2016-11-27 18:50:16 -0800

    Modularize Process.py

--- NaiveBayes/Process.py
+++ NaiveBayes/Process.py
@@ -6,94 +6,96 @@ import re
 import pandas as pd
 import numpy as np
 import word_category_counter
+import argparse
 
-def countWords(row):
-	Class = row[1]
-	for word in row[0]:
-		bow[Class][word] = bow[Class].get(word,0) + 1.0
-		N_words_[Class]+=1
+def naiveBayes(data, testData):
+	def countWords(row):
+		Class = row[1]
+		for word in row[0]:
+			bow[Class][word] = bow[Class].get(word, 0) + 1.0
+			n_words_[Class] += 1
 
-def applyNaiveBayes(row):
-	Class = row[1]
-	B = 0
-	C = 0
-	#algorithm
-	for word in row[0]:
-		if word in bow[1]:
-			B += math.log(bow[1][word],10)
+	def applyNaiveBayes(row):
+		sumOfClass1 = 0
+		sumOfClass2 = 0
+		# algorithm
+		for word in row[0]:
+			if word in bow[1]:
+				sumOfClass1 += math.log(bow[1][word], 10)
+			else:
+				sumOfClass1 += math.log(1.0 / (n_words_[1] + uniqueWords), 10)
+
+			if word in bow[0]:
+				sumOfClass2 += math.log(bow[0][word], 10)
+			else:
+				sumOfClass2 += math.log(1.0 / (n_words_[0] + uniqueWords), 10)
+		# print A,B,C
+		# classification
+		if priorPortion + sumOfClass1 - sumOfClass2 > 0:
+			t = 1
 		else:
-			B += math.log(1.0/(N_words_[1]+wordB),10)
+			t = 0
+		return t
+	nbData = data[['text', 'sentiment']]
+	nbData_1 = nbData[nbData.sentiment == 1]
+	nbData_0 = nbData[nbData.sentiment == 0]
 
-		if word in bow[0]:
-			C += math.log(bow[0][word],10)
-		else:
-			C += math.log(1.0/(N_words_[0]+wordB),10)
-	#print A,B,C
-	#classification
-	t=-1
-	if A+B-C >0:
-		t=1
-	else:
-		t=0
-	#accuracy
-	global TruePositive
-	global TrueNegative
-	global FalsePositive
-	global FalseNegative
-	if t==Class:
-		if t==1:
-			TruePositive+=1
-		else:
-			TrueNegative+=1
-	else:
-		if t==1:
-			FalsePositive+=1
-		else:
-			FalseNegative+=1
+	bow = [{}, {}]
+	# number of docs
+	n = nbData.count()[0]
+	# number of docs in each class
+	n_ = [nbData_0.count()[0], nbData_1.count()[0]]
 
-if( __name__ == '__main__'):
-	#load feature vectors
-	Data = pd.read_pickle('NaiveBayesData.pkl')
-	NBData = Data[['text','sentiment']]
-	NBData_1 = NBData[NBData.sentiment==1]
-	NBData_0 = NBData[NBData.sentiment==0]
-
-
-	bow = [{},{}]
-	#number of docs
-	N = NBData.count()[0]
-	#number of docs in each class
-	N_ = [NBData_0.count()[0], NBData_1.count()[0]]
-
-	#prior
-	Prior = [N_[0]/float(N), N_[1]/float(N)]
+	# prior
+	prior = [n_[0] / float(n), n_[1] / float(n)]
 	##total words for each class
-	N_words_ = [0, 0]
+	n_words_ = [0, 0]
 
-	NBData.apply(countWords, axis = 1)
+	nbData.apply(countWords, axis=1)
 
-	#unique words for all classes
-	wordB=len(set(bow[0].keys()+bow[1].keys()))
-	#laplace smoothing
+	# unique words for all classes
+	uniqueWords = len(set(bow[0].keys() + bow[1].keys()))
+	# laplace smoothing
 	for i in range(len(bow)):
 		for word in bow[i]:
-			bow[i][word]=(float(bow[i][word])+1)/(N_words_[i]+wordB)
+			bow[i][word] = (float(bow[i][word]) + 1) / (n_words_[i] + uniqueWords)
 
-	#generate test data
-	TestData = pd.read_csv('NaiveBayesData2.csv')
+	# generate test data
 
-	A = math.log(Prior[1],10)-math.log(Prior[0],10)
-	TruePositive = 0
-	FalsePositive = 0
-	TrueNegative = 0
-	FalseNegative = 0
+	priorPortion = math.log(prior[1], 10) - math.log(prior[0], 10)
 	# A -> prior , B -> sum of the class 1, C -> sum of the class 0
-	#run test
-	NBTestData = TestData[['text','sentiment']]
-	NBTestData.apply(applyNaiveBayes, axis=1)
+	# run test
+	nbTestData = testData[['text', 'sentiment']]
+	predictLabel = nbTestData.apply(applyNaiveBayes, axis=1)
 
-	#print accuracy
-	print "True Positive:  ", TruePositive
-	print "False Positive: ", FalsePositive
-	print "True Negative:  ", TrueNegative
-	print "False Negative: ", FalseNegative
+	# print accuracy
+	print "True Positive:  ", predictLabel[predictLabel == 1][predictLabel == nbTestData['sentiment']].count()
+	print "False Positive: ", predictLabel[predictLabel == 1][predictLabel != nbTestData['sentiment']].count()
+	print "True Negative:  ", predictLabel[predictLabel == 0][predictLabel == nbTestData['sentiment']].count()
+	print "False Negative: ", predictLabel[predictLabel == 0][predictLabel != nbTestData['sentiment']].count()
+	return predictLabel
+
+def init():
+  parser = argparse.ArgumentParser(description="Specify feature types")
+  parser.add_argument("-u", "--unigram", help="activate the unigram feature",
+                      action="store_true")
+  parser.add_argument("-b", "--bigram", help="activate the bigram feature",
+                      action="store_true")
+  parser.add_argument("-l", "--liwc", help="activate the LIWC feature",
+                      action="store_true")
+  parser.add_argument("-a", "--all", help="create maximum combos and pickle each",
+                      action="store_true")
+  parser.add_argument("-t", "--tfidf", help="use tfidf frequency count",
+                      action="store_true")
+  parser.add_argument("-n", "--naive_bayes", help="use naive bayes classifier",
+                      action="store_true")
+  return parser.parse_args()
+if( __name__ == '__main__'):
+	#load feature vectors
+	args = init()
+	data = pd.read_csv('NaiveBayesData.csv')
+	testData = pd.read_csv('NaiveBayesData2.csv')
+	if args.naive_bayes:
+		nbTestData = naiveBayes(data, testData)
+
+
